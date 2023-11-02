f94811e7d426371ddf47026a7f37b375e3bf8943
YaMiN <yamin8000@yahoo.com>
2023-11-02 20:12:09 +0330

    fixed some entities

--- Authentication.Domain/Entities/Verification.cs
+++ Authentication.Domain/Entities/Verification.cs
@@ -3,6 +3,8 @@
 public class Verification : BaseEntity
 {
     public required string Code { get; set; }
-    
+
+    public bool IsVerified { get; set; }
+
     public UserChannel? UserChannel { get; set; }
 }
