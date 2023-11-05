37b33f62bf4de22425abb9971852384e307e6161
Arian Tron <ariantron@yahoo.com>
2023-11-05 10:54:35 +0330

    Add UpdateOrCreate Function To Repository Classes

--- Authentication.Infrastructure/Interfaces/ICrudRepository.cs
+++ Authentication.Infrastructure/Interfaces/ICrudRepository.cs
@@ -8,6 +8,7 @@ public interface ICrudRepository<TEntity> where TEntity : BaseEntity
     Task<IEnumerable<TEntity?>> GetAllAsync();
     Task<TEntity?> CreateAsync(TEntity? entity);
     Task<TEntity?> UpdateAsync(TEntity? entity);
+    Task<TEntity?> UpdateOrCreateAsync(TEntity? entity, List<(string, string)> attributes);
     Task<bool> DeleteAsync(Guid id);
     Task<bool> RestoreAsync(Guid id);
     Task<bool> ForceDeleteAsync(Guid id);
